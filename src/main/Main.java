98ba7276f84bd12fe400d89db39202d7fdb7cfa0
大城　慶知 <e165745@ie.u-ryukyu.ac.jp>
2016-11-30 22:53:26 +0900

    accustom exception

---
+++ src/main/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by e165745 on 2016/11/30.
+ */
+public class Main  {
+    public static void main(String[] args) {
+        String str = null;
+        System.out.println(str.length());
+    }
+
+}
