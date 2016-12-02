0e794819b1f44e9bcea8b62f993a1f3a9b5bc7d0
大城　慶知 <e165745@ie.u-ryukyu.ac.jp>
2016-12-02 18:53:18 +0900

    accustom exception

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,8 +5,9 @@
 public class Main  {
     public static void main(String[] args) {
 
-            String str = "3．14";
+            String str = "3.　14";
             Double value = Double.parseDouble(str);
 
+        
     }
 }
